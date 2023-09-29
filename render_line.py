07c0042e4aeb111d4688975d8e0c52bdbf149062
Herb0310 <70586406+Herb0310@users.noreply.github.com>
2023-09-29 10:21:36 +0900

    Update render_line.py

--- render_line.py
+++ render_line.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage,
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 handler = LineBotApi(os.environ["LINE_SECRET"])
 line_bot_api = WebhookHandler(os.environ["LINE_TOKEN"])
